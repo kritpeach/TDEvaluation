c94ee928fd074e8fccf660d44b6df69ac7690d48
Krit Chernkwansri <getpro1@gmail.com>
2018-06-28 15:11:07 +0700

    Add Evaluation response page

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -5,9 +5,9 @@ import play.api._
 import play.api.mvc._
 
 /**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
+  * This controller creates an `Action` to handle HTTP requests to the
+  * application's home page.
+  */
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
@@ -15,20 +15,29 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
   }
+
   def signIn() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.signin())
   }
+
   def managementUserList() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.managementUserList())
   }
+
   def managementEvaluationList() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.managementEvaluationList())
   }
+
   def managementEvaluationEdit(id: Long) = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.managementEvaluationEdit())
   }
+
   def managementEvaluationView(id: Long) = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.managementEvaluationView())
+  }
+
+  def managementEvaluationResponse(evaluationFormId: Long, userId: Long) = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.managementEvaluationResponse())
   }
 
 }
