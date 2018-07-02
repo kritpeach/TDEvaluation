3544200b1d7600c196318848795c0a466bb78f7a
Krit Chernkwansri <5831305003@lamduan.mfu.ac.th>
2018-07-02 17:25:03 +0700

    - Create table from slick schema - User list to json

--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -2,9 +2,14 @@ package controllers
 
 import dao.UserDAO
 import javax.inject._
+import models.User
 import play.api.i18n.I18nSupport
-import play.api.libs.json.Json
+import play.api.libs.json.{Json, Writes}
 import play.api.mvc._
+
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+// import scala.async.Async.{async, await}
 
 import scala.concurrent.ExecutionContext
 
@@ -18,7 +23,15 @@ class UserController @Inject()(
     Ok(views.html.managementUserList())
   }
 
-  def userList() = Action.async {
-    userDAO.all().map { case (x) => Ok(x(1).username) }
+  def createTable() = Action {
+    Await.result(userDAO.createTable, Duration.Inf)
+    Ok("Create Table")
+  }
+
+  def userList() = Action { implicit request =>
+    // val user = User(None,"peachkrit","987654321",true)
+    implicit val userFormat = Json.format[User]
+    val users = Await.result(userDAO.all, Duration.Inf)
+    Ok(Json.toJson(users))
   }
 }
--- app/dao/UserDAO.scala
+++ app/dao/UserDAO.scala
@@ -10,13 +10,19 @@ import models.User
 class UserDAO @Inject()(protected val dbConfigProvider: DatabaseConfigProvider)(implicit executionContext: ExecutionContext) extends HasDatabaseConfigProvider[JdbcProfile] {
 
   import profile.api._
+
   private val Users = TableQuery[UsersTable]
   def all(): Future[Seq[User]] = db.run(Users.result)
+  def insert(user: User) = db.run(Users += user).map(_ => ())
+  def createTable = db.run(Users.schema.create)
 
   private class UsersTable(tag: Tag) extends Table[User](tag, "USER") {
     def id = column[Long]("ID", O.PrimaryKey, O.AutoInc)
+
     def username = column[String]("USERNAME")
+
     def password = column[String]("PASSWORD")
+
     def isManager = column[Boolean]("IS_MANAGER")
 
     def * = (id.?, username, password, isManager) <> (User.tupled, User.unapply)
