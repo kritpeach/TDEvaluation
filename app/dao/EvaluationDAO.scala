e5862b03fe7b0b9a36b0abe6aa7e349f629cb128
Krit Chernkwansri <5831305003@lamduan.mfu.ac.th>
2018-07-06 17:15:04 +0700

    Add evaluation model and DAO

---
+++ app/dao/EvaluationDAO.scala
@@ -0,0 +1,48 @@
+package dao
+
+import java.sql.Timestamp
+
+import javax.inject.Inject
+import models.{Evaluation, Evaluation}
+import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
+import slick.jdbc.JdbcProfile
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class EvaluationDAO @Inject()(protected val dbConfigProvider: DatabaseConfigProvider)(implicit executionContext: ExecutionContext) extends HasDatabaseConfigProvider[JdbcProfile] {
+
+  import profile.api._
+
+  private val Evaluations = TableQuery[EvaluationsTable]
+
+  def list(): Future[Seq[Evaluation]] = db.run(Evaluations.sortBy(_.id).result)
+
+  def insert(evaluation: Evaluation): Future[Evaluation] = db
+    .run(Evaluations returning Evaluations.map(_.id) += evaluation)
+    .map(id => evaluation.copy(id = Some(id)))
+
+  def delete(id: Long): Future[Int] = db.run(Evaluations.filter(_.id === id).delete)
+
+  def upsert(evaluation: Evaluation): Future[Evaluation] = db.run((Evaluations returning Evaluations).insertOrUpdate(evaluation)).map {
+    case None => evaluation
+    case Some(u) => evaluation.copy(id = u.id)
+  }
+  def createTable: Future[Unit] = db.run(Evaluations.schema.create)
+
+  private class EvaluationsTable(tag: Tag) extends Table[Evaluation](tag, "USER") {
+    def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
+
+    def title = column[String]("title")
+
+    def titleIndex = index("index_title", title, unique = true)
+
+    def enabled = column[Boolean]("enabled")
+
+    def createAt = column[Timestamp]("create_at")
+
+    def creator = column[Long]("creator")
+
+    def * = (id.?, title, enabled, createAt, creator) <> ((Evaluation.apply _).tupled, Evaluation.unapply)
+  }
+
+}
---
+++ app/models/Evaluation.scala
@@ -0,0 +1,19 @@
+package models
+
+import java.sql.Timestamp
+
+import org.joda.time.DateTime
+import play.api.libs.json.{Json, OFormat, Reads}
+
+case class Evaluation(
+                       id: Option[Long] = None,
+                       title: String,
+                       enabled: Boolean = true,
+                       createAt: Timestamp = new Timestamp(DateTime.now.getMillis),
+                       creator: Long
+                     )
+
+object Evaluation {
+  implicit val evaluationFormat: OFormat[Evaluation] = Json.format[Evaluation]
+  implicit val evaluationReads: Reads[Evaluation] = Json.reads[Evaluation]
+}
