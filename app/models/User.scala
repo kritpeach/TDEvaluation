81eacc341ef0f4f7757f10e532d369148086bc9e
Krit Chernkwansri <5831305003@lamduan.mfu.ac.th>
2018-07-06 17:14:19 +0700

    Add type

--- app/models/User.scala
+++ app/models/User.scala
@@ -1,10 +1,10 @@
 package models
 
-import play.api.libs.json.Json
+import play.api.libs.json.{Json, OFormat, Reads}
 
 case class User(id: Option[Long] = None, username: String, password: String, isManager: Boolean)
 
 object User {
-  implicit val userFormat = Json.format[User]
-  implicit val userReads = Json.reads[User]
+  implicit val userFormat: OFormat[User] = Json.format[User]
+  implicit val userReads: Reads[User] = Json.reads[User]
 }
